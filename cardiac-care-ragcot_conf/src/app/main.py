78b978b25ef0b6c5aafdb12bb1d14b692bf0a906
Harry5174 <harisjaved010@gmail.com>
2025-05-12 22:46:25 +0500

    Refactor main.py to enable multiprocessing and integrate Chainlit with FastAPI

--- cardiac-care-ragcot_conf/src/app/main.py
+++ cardiac-care-ragcot_conf/src/app/main.py
@@ -4,6 +4,5 @@ import uvicorn
 import multiprocessing as mp
 
 if __name__ == "__main__":
-    # mp.set_start_method("fork", force=True)
-    
+    mp.set_start_method("fork", force=True)
     uvicorn.run(app=app, host="localhost", port=8000, log_level="info")
--- cardiac-care-ragcot_conf/src/app/routers/routes_main.py
+++ cardiac-care-ragcot_conf/src/app/routers/routes_main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI, Depends
 from app.routers import routes_gemini
+from chainlit.utils import mount_chainlit
 
 app = FastAPI()
 app.include_router(routes_gemini.router, prefix="/api", tags=["Gemini"])
+
+
+mount_chainlit(
+    app=app,
+    target="chainlit_app.py",
+    path="/chainlit",
+)
